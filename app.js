79e65f9928c8f8e856e788edb64bbb9d089f408d
ryanloftus <ryanloftus02@outlook.com>
2021-08-02 20:25:04 -0400

    Added a chart to display one year data

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const Chart = require('./node_modules/chart.js');
+
 const url = 'https://finnhub.io/api/v1/';
 const quoteParam = 'quote?symbol=';
 const candleParam = 'stock/candle?symbol=';
@@ -5,31 +7,45 @@ const apiKey = '&token=c41hlviad3ies3kt3gmg';
 
 const ticker = document.getElementById('ticker');
 const submit = document.getElementById('submit');
+let candleGraph = makeGraph();
 
 const blankVal = '--';
+
+function makeGraph() {
+    return new Chart(document.getElementById('candle'), {
+        type: 'line', 
+        data: {
+            labels: [], 
+            datasets: [{
+                label: 'Value',
+                data: [], 
+                fill: false,
+                borderColor: 'rgb(75, 192, 192)'
+            }]
+        }
+    });
+}
 
 function getDateParams() {
     const now = Math.floor(new Date().getTime() / 1000);
     const oneYearAgo = now - 365 * 24 * 60 * 60;
-    //TODO: can get max date range with 0 instead of oneYearAgo
+    //TODO: make date range configurable
     return '&resolution=D&from=' + oneYearAgo + '&to=' + now;
+}
+
+//TODO: remove day of week from date
+function getReadableDates(dates) {
+    let readableDates = [];
+    dates.forEach(date => readableDates.push(new Date(date * 1000).toDateString()));
+    return readableDates;
 }
 
 function getDollarVal(val) {
     return (val ? (Math.round(val * 100) / 100).toFixed(2) : blankVal);
 }
 
-function getCandleTableHTML(close, volume) {
-    let tableHTML = '';
-    for (let i = 0; i < close.length; i++) {
-        tableHTML += '<tr><td>' + getDollarVal(close[i]) + '</td><td>' + volume[i] + '</td></tr>';
-    }
-    return tableHTML;
-}
-
 function renderQuote(quote) {
     const responseField = document.getElementById('quote');
-    console.log(JSON.stringify(quote));
     responseField.innerHTML = '<p>' +
         'Current Price: \$' + getDollarVal(quote.c) +
         '<br><br>Change: \$' + getDollarVal(quote.d) +
@@ -62,10 +78,9 @@ async function getStockQuote() {
 
 function renderCandle(candle) {
     if (candle.s === 'ok') {
-        const responseField = document.getElementById('candle');
-        responseField.innerHTML = '<table><thead><tr><th>Close</th><th>Volume</th></tr></thead>' +
-            getCandleTableHTML(candle.c, candle.v) +
-            '</table>';
+        candleGraph.data.labels = getReadableDates(candle.t);
+        candleGraph.data.datasets[0].data = candle.c;
+        candleGraph.update();
     }
 }
 
@@ -88,7 +103,8 @@ async function getStockCandle() {
     }
 }
 
-// TODO: remove unresolved promises when search is clicked
+// TODO: remove unresolved promises when search is clicked?
+// TODO: restructure code so I have consts pointing to each element and I set the value for that element with a setter
 async function displayStockQuote() {
     getStockQuote();
     getStockCandle();
